464917b6a07ea0e50531c75509d27cdba0b7adbe
akilashj <shashiduniakila98@outlook.com>
2025-12-03 16:34:54 +0200

    Select a Route window of the game.

--- Js/PortScript.js
+++ Js/PortScript.js
@@ -1,4 +1,12 @@
-const portsContainer = document.getElementById("ports");
-const map = document.getElementById("map");
+const portsContainer = document.getElementById("Ports");
+const map = document.getElementById("Map");
 
-map.onload = ()
+map.onload = () => {
+    for (let i = 0; i < 10; i++) {
+        const port = document.createElement("div");
+        port.classList.add("Port");
+
+
+        }
+
+}
