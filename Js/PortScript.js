0689f0fb092549c0655d62537c537c8f70cabd96
akilashj <shashiduniakila98@outlook.com>
2025-12-04 13:50:48 +0200

    Select a Route window of the game.

--- Js/PortScript.js
+++ Js/PortScript.js
@@ -31,10 +31,10 @@ for (let i = 0; i < 10; i++) {
   const x = Math.random() * 90 + 5;
   const y = Math.random() * 90 + 5;
 
-        port.style.left = x + "%";
-        port.style.top = y + "%";
+  port.style.left = x + "%";
+  port.style.top = y + "%";
 
-        port.addEventListener("Click", () => {
+  port.addEventListener("Click", (e) => {
              alert(`Port Selected! (#${i + 1})`);
         });
 
