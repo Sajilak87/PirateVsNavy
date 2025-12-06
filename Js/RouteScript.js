6fba7fb4b7c5af4f545fca9660a19c53c3907704
akilashj <shashiduniakila98@outlook.com>
2025-12-06 19:04:26 +0200

    Available Routes window of the game.

--- Js/RouteScript.js
+++ Js/RouteScript.js
@@ -6,3 +6,13 @@ routeLinks.forEach((link, index) => {
         alert(`Route ${index + 1} selected!`);
     });
 });
+
+const routesData = await routesRes.json();
+        console.log("Generated routes:", routesData);
+        alert(`Routes fetched! Check console for details.`);
+
+      } catch (err) {
+        console.error("API error:", err);
+        alert("Error calling APIs. Check console.");
+      }
+    });
